db055e9a2249b76d83cb289799a79d7fff780c1a
MarsMoon544 <34398758+MarsMoon16@users.noreply.github.com>
2019-03-01 16:25:28 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<?php 
+
+header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+header('Expires: 0');
+
+?>
+
+<head>
+<title>Fungi's City Hall</title>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+
+</head>
+<body>
+
+<body>
+
+<!-- Top -->
+<div class="top">
+<h1>Welcome to Fungi's City Hall</h1>
+<textarea>Salut 
+</textarea>
+</div>
+
+
+<!-- Content -->
+<div class="content">
+</div>
+
+
+<!-- Bottom -->
+<div class="bottom">
+</div>
+
+
+</body>
+</html>
